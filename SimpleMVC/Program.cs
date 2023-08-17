6218bb5284f6a661efa69a0f37086de8c9a91df7
BScodeMk <bscodemk@outlook.com>
2023-08-17 21:07:17 +0200

    Add project files.

---
+++ SimpleMVC/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using SimpleMVC.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<SimpleMvcDbContext>(option => option.UseSqlServer(connectionString));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Start}/{action=Index}/{id?}");
+
+app.Run();
