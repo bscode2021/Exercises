afb039b055c8efe41470ecbd670f47610724dafc
BScodeMk <bscodemk@outlook.com>
2023-09-12 20:53:19 +0200

    Added category

---
+++ Ogani/Data/Entities/Category.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ogani.Data.Entities
+{
+    [Table("tbl_categories")]
+    public class Category
+    {
+        public Category()
+        {
+            CategoryID = new Guid();
+            CategoryName = String.Empty;
+            CategoryDescription = String.Empty;
+        }
+
+        public Category(string categoryName, string categoryDescription)
+        {
+            CategoryID = new Guid();
+            CategoryName = categoryName ?? throw new ArgumentNullException(nameof(categoryName));
+            CategoryDescription = categoryDescription ?? throw new ArgumentNullException(nameof(categoryDescription));
+        }
+
+        [Key]
+        public Guid CategoryID { get; set; }
+
+        [StringLength(150, ErrorMessage = "Category name to long. Must be less then 150 characters!")]
+        public string CategoryName { get; set; }
+
+
+        [StringLength(500, ErrorMessage = "Category description  to long. Must be less then 500 characters!")]
+        public string CategoryDescription { get; set; }
+
+        
+    }
+}
